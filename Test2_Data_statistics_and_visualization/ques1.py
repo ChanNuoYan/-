5de96bf8cab1f5553f494cdb6fc4499f93591fd2
ChanNuoYan <54059043+ChanNuoYan@users.noreply.github.com>
2020-12-31 09:34:23 +0800

    Update ques1.py

--- Test2_Data_statistics_and_visualization/ques1.py
+++ Test2_Data_statistics_and_visualization/ques1.py
@@ -1,22 +1,34 @@
 import numpy as np
 from matplotlib import pyplot as plt
-def turnscore(data):#将Object类型数据转换为数值数据
+
+
+def turnscore(data):  # 将Object类型数据转换为数值数据
+    """
+    :param data: 原数据表
+    :return: return none
+    """
     for i in data.columns:
-        if i=="Constitution":
+        if i == "Constitution":
             for j in range(106):
-                if data[i][j]=="bad":
-                    temp=np.random.randint(0,60,1)
-                    data[i][j]=temp[0]
-                elif data[i][j]=="general":
-                    temp = np.random.randint(60,80, 1)
+                if data[i][j] == "bad":
+                    temp = np.random.randint(0, 60, 1)
                     data[i][j] = temp[0]
-                elif data[i][j]=="good":
-                    temp = np.random.randint(80,90, 1)
+                elif data[i][j] == "general":
+                    temp = np.random.randint(60, 80, 1)
                     data[i][j] = temp[0]
-                elif data[i][j]=="excellent":
+                elif data[i][j] == "good":
+                    temp = np.random.randint(80, 90, 1)
+                    data[i][j] = temp[0]
+                elif data[i][j] == "excellent":
                     temp = np.random.randint(90, 101, 1)
                     data[i][j] = temp[0]
-def ques1(data):#绘图表示C1和Constitution
+
+
+def ques1(data):  # 绘图表示C1和Constitution
+    """
+    :param data: 原数据表
+    :return: return none
+    """
     x = data["C1"]
     y = data["Constitution"]
     plt.title("Matplotlib demo")
