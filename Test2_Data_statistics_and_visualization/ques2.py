35c39d9317e0d452e32df3ff37034060a328b184
ChanNuoYan <54059043+ChanNuoYan@users.noreply.github.com>
2020-12-31 09:34:04 +0800

    Update ques2.py

--- Test2_Data_statistics_and_visualization/ques2.py
+++ Test2_Data_statistics_and_visualization/ques2.py
@@ -1,6 +1,12 @@
 from matplotlib import pyplot as plt
-def ques2(data):#绘图
-    plt.hist(x=data.C1,bins=20,color='steelblue',edgecolor = 'black',range=(0,100))
+
+
+def ques2(data):  # 绘图
+    """
+    :param data: 原数据表
+    :return: return none
+    """
+    plt.hist(x=data.C1, bins=20, color='steelblue', edgecolor='black', range=(0, 100))
     plt.xlabel("C1")
     plt.ylabel("Score")
     plt.title("Score Histogram Of C1")
