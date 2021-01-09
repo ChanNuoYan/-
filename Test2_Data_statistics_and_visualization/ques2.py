13f032d82aae558bf9ca38036d7075684467e6ac
ChanNuoYan <54059043+ChanNuoYan@users.noreply.github.com>
2021-01-09 14:50:10 +0800

    Update ques2.py

--- Test2_Data_statistics_and_visualization/ques2.py
+++ Test2_Data_statistics_and_visualization/ques2.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/11/18 10:43
+# @Author  : Adam
+# @Site    : 
+# @File    : Log.py
+# @Software: PyCharm
 from matplotlib import pyplot as plt
 
 
